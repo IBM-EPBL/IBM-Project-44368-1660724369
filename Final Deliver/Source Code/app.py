a91a5a555ce2d941e5e6451d2fa0d67299db074a
Mohamedhuthaiba <112994704+Mohamedhuthaiba@users.noreply.github.com>
2022-11-19 18:00:53 +0530

    Update app.py

--- Final Deliver/Source Code/app.py
+++ Final Deliver/Source Code/app.py
@@ -10,7 +10,7 @@ app.secret_key = 'a'
 conn= ibm_db.connect("DATABASE=bludb;HOSTNAME=b70af05b-76e4-4bca-a1f5-23dbb4c6a74e.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32716;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=bhb82079;PWD=r6yXgwLlAJHl3nFC",'','')
   
 
-#newsapi = NewsApiClient(api_key='53015b6413a14519b959379521c861d4')
+newsapi = NewsApiClient(api_key='53015b6413a14519b959379521c861d4')
   
 
 def get_sources_and_domains():
